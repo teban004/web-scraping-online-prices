7d353bcbcb9b844705aba994fb77d4cc4285b89b
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-11-16 16:36:06 -0500

    added load and parse the HTML

--- phonePrice.py
+++ phonePrice.py
@@ -1,3 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+#get the data from the website
+website = 'https://www.apple.com/ca/iphone-se/'
+data = requests.get(website)
+
+print("The response from", website, "is", data)
+print()
+
+#load data into bs4
+soup = BeautifulSoup(data.text, 'html.parser')
+
