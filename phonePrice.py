572c3517965b0a9aa22c005db54b6b895162926b
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-11-16 16:49:06 -0500

    scraped the HTNL to get the desired element

--- phonePrice.py
+++ phonePrice.py
@@ -2,12 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 #get the data from the website
-website = 'https://www.apple.com/ca/iphone-se/'
-data = requests.get(website)
+URL = 'https://www.apple.com/ca/iphone-se/'
+data = requests.get(URL)
 
-print("The response from", website, "is", data)
+print("The response from", URL, "is", data)
 print()
 
 #load data into bs4
-soup = BeautifulSoup(data.text, 'html.parser')
+soup = BeautifulSoup(data.content, 'html.parser')
+
+#get data by getting the span element inside div with class hero-description-group
+data =[]
+priceDiv = soup.find('div', {'class': 'hero-description-group'})
+priceSpan = priceDiv.find("span")
+print(priceSpan.text)
 
